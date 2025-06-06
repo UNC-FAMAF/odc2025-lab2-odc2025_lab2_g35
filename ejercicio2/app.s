35f44994e8e6798e03687a0bf1290a74327f2608
Tsue <tsue.motosono.pereda@mi.unc.edu.ar>
2025-06-06 20:25:52 -0300

    Update app.s

--- ejercicio2/app.s
+++ ejercicio2/app.s
@@ -1,53 +1,53 @@
-	.equ SCREEN_WIDTH, 		640
-	.equ SCREEN_HEIGH, 		480
-	.equ BITS_PER_PIXEL,  	32
+.equ SCREEN_WIDTH, 640
+ .equ SCREEN_HEIGH, 480
+ .equ BITS_PER_PIXEL, 32
 
-	.equ GPIO_BASE,      0x3f200000
-	.equ GPIO_GPFSEL0,   0x00
-	.equ GPIO_GPLEV0,    0x34
+ .equ GPIO_BASE, 0x3f200000
+ .equ GPIO_GPFSEL0, 0x00
+ .equ GPIO_GPLEV0, 0x34
 
-	.globl main
+ .globl main
 
 main:
-	// x0 contiene la direccion base del framebuffer
- 	mov x20, x0	// Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
+ mov x20, x0 // Guarda la dirección base del framebuffer en x20 (x0 contiene la direccion base del framebuffer)
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+ //---------------------------------------------- CIELO ---------------------------------------------------------//
+ movz x15, 0x95ED, lsl 00
+ movk x15, 0xFF64, lsl 16 // Color ARGB: celeste cielo
 
-	mov x2, SCREEN_HEIGH         // Y Size
+ mov x2, SCREEN_HEIGH/2 // Y Size
 loop1:
-	mov x1, SCREEN_WIDTH         // X Size
+ mov x1, SCREEN_WIDTH // X Size
 loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
+ stur w15,[x0] // Colorear el pixel N
+ add x0,x0,4 // Siguiente pixel
+ sub x1,x1,1 // Decrementar contador X
+ cbnz x1,loop0 // Si no terminó la fila, salto
+ sub x2,x2,1 // Decrementar contador Y
+ cbnz x2,loop1 // Si no es la última fila, salto
 
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
+ //----------------------------------------- PASTO -------------------------------------------------------------//
+ movz x15, 0x8E23, lsl 0
+ movk x15, 0xFF6B, lsl 16 // color verde oliva
+ mov x0, x20
+ mov x10, #640
+ mov x11, #240
+ mul x11, x11, x10 // x11 = 240 * 640
+ lsl x11, x11, #2 // x11 *= 4 → offset en bytes
+ add x0, x0, x11 // x0 apunta a la mitad de la pantalla
+ 
+ mov x2, SCREEN_HEIGH/2 // Y Size
+loop1b:
+ mov x1, SCREEN_WIDTH // X Size
+loop0b:
+ stur w15,[x0] // Colorear el pixel N
+ add x0,x0,4 // Siguiente pixel
+ sub x1,x1,1 // Decrementar contador X
+ cbnz x1,loop0b // Si no terminó la fila, salto
+ sub x2,x2,1 // Decrementar contador Y
+ cbnz x2,loop1b // Si no es la última fila, salto
 
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
-
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
-
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
-
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
-
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
-
-	//---------------------------------------------------------------
-	// Infinite Loop
+ mov x0, x20
 
 InfLoop:
 	b InfLoop
